6440fa6e61b46814173adcd7b7ec79561363b91f
Ohanronnnie <titiloyepaul68@gmail.com>
2023-01-28 19:14:08 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import './index.css';
 // Bootstrap CSS
 import App from './App';
 export default function Note(){
-	return <App/>
+	return(
+	<>
+	 <App/>
+         <div id="container-901e95f448a677e0972cc42c2146c468"></div> 
+        </>
+	)
 }
 ReactDOM.render(
   <React.StrictMode>
+
     <Note />
   </React.StrictMode>,
   document.getElementById('root')
